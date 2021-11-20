8fe75033580a68a000431ed63a468325aed59c0f
Mohamad Alsyouf <momogotswag99@gmail.com>
2021-11-20 02:09:56 -0800

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,10 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+for (var t = 0; t < 20; t += 2) {
+  console.log(t);
+}
+for (var s = 100; s > 0; s--) {
+  console.log('Time till explosion ' + s + '!');
+}
